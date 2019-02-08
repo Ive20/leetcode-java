9f3f8edfddc7f8230671b92eacdb57b5a170f1d0
maxliu <maxliu@kucoin.com>
2019-02-08 14:37:15 +0800

    3

--- Run.java
+++ Run.java
@@ -1,3 +1,5 @@
+import three.Solution;
+
 /**
  * Run
  */
@@ -5,5 +7,9 @@ public class Run {
 
     public static void main(String[] args) {
         System.out.println("Aaa");
+        String s = "abca";
+        Solution sol = new Solution();
+        int r = sol.lengthOfLongestSubstring(s);
+        System.out.println(r);
     }
 }
---
+++ three/Solution.java
@@ -0,0 +1,29 @@
+package three;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            List<Character> used = new ArrayList<>();
+            if (result >= s.length() - i) {
+                break;
+            }
+            int temp =0;
+            for (int j = i; j < s.length(); j++) {
+                if (used.contains(s.charAt(j))) {
+                    break;
+                } else {
+                    temp ++;
+                    if (result < temp) {
+                        result = temp;
+                    }
+                    used.add(s.charAt(j));
+                }
+            }
+        }
+        return result;
+    }
+}
